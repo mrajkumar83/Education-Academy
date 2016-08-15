57f055991c30395f15c858c7451756d6e441d1ab
mrajkumar83 <mrajkumar83@gmail.com>
2016-08-15 14:12:05 +0200

    Vehicle display

--- admin/manage_vehicle_details.php
+++ admin/manage_vehicle_details.php
@@ -10,7 +10,7 @@ require_once('includes/common.php');
 $db = new Query();
 // it has to be with the branch for time being do it
 //$user = $db->fetchRecord(' tb_users ', ' user_branch ', ' user_id = "' . $_SESSION['UID'] . '"');
-$cond = (isset($UTYPE) && $UTYPE == 'SA') ? NULL : ' vehicle_category = category_id ';//$user->user_branch
+$cond = ' vehicle_category = category_id '.((isset($UTYPE) && $UTYPE == 'SA') ? '' : ' AND vehicle_createdby = "' . $_SESSION['UID'] . '" ');//$user->user_branch
 $vehicle = $db->fetchAllRecord(' tb_vehicle_details , tb_vehicle_categories ', ' vehicle_id, vehicle_name, category_name, vehicle_status ', $cond, NULL, null, NULL, 0, 'All');
 ?>
 <div id="main">
@@ -31,10 +31,9 @@ $vehicle = $db->fetchAllRecord(' tb_vehicle_details , tb_vehicle_categories ', '
                 echo '<tr class="', $class, '">', "\n\t";
                 echo '<td>', $data->vehicle_name, '</td>', "\n";
 				echo '<td>', $data->category_name, '</td>', "\n";
-                echo '<td class="center">', ($data->vehicle_status == 'A' ? '' : 'Not-'), 'Working</td>';
+                echo '<td class="center">', ($data->vehicle_status == 'W' ? '' : 'Not-'), 'Working</td>';
                 echo '<td class="center"><a href="vehicle.php?op=E&amp;id=', $data->vehicle_id, '" title="Edit Vehicle" alt="Edit Vehicle"><div class="img edit"></div></a>&nbsp;';
                 echo '<a href="../admin/logic/vehicle_logic.php?op=D&amp;id=', $data->vehicle_id, '" title="Delete vehicle" alt="Delete vehicle"><div class="img delete"></div></a>';
-                
                 echo '</td></tr>', "\n";
                 $row++;
             }
